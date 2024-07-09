4ca85bef35b01b67f2bd8fffac89ebd6c0e014f8
Ebhodaghe, Joel <jebhodaghe@libertyglobal.com>
2024-07-09 08:32:29 +0000

    Add new file

---
+++ security_groups.tf
@@ -0,0 +1,44 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+output "web_sg_id" {
+  value = aws_security_group.web_sg.id
+}
